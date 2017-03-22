1f585fa9a96089f94e1bd44be690d7e9f240a483
Ben Houghton <benho@microsoft.com>
2017-03-22 11:55:27 +0000

    Initial Commit

---
+++ AzureForensicsFunctions/ImageAnalysisQueueTriggerNodeJS/index.js
@@ -0,0 +1,43 @@
+﻿
+var request = require('request');
+var uuidV1 = require('uuid/v1');
+
+module.exports = function (context, myQueueItem, outputDocument, myOutQueueItem) {
+
+    context.log('Node.js queue trigger function processed work item', myQueueItem);
+
+    var queryString = 'visualFeatures=Categories,Tags,Description,Faces,ImageType,Color,Adult&language=en';
+
+    var documentId = uuidV1();
+
+    request.post({
+        url: 'https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?'+ queryString,
+        headers: {
+            "Content-Type": "application/json",
+            "Ocp-Apim-Subscription-Key": process.env.SubscriptionKey
+        },
+        json: { "url": myQueueItem }
+        },
+        function (err, res, body) {
+            if (err)
+            {
+                console.log('Error Analysing Image:', err);
+            }
+            //Check for right status code
+            if (res.statusCode !== 200) {
+                console.log('Invalid Status Code Returned:', res);
+            }
+            else {
+              
+
+                context.bindings.outputDocument = body;
+                context.bindings.outputDocument.id = documentId;
+                context.bindings.outputDocument.imageUrl = myQueueItem;
+                context.log('Written to DocumentDB', context.bindings.outputDocument);
+                context.bindings.myOutQueueItem = documentId;
+                context.log('OutputQueue', documentId);
+                context.done();
+            }
+        });
+
+}
